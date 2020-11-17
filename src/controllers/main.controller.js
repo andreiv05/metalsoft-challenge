6ba20aee3d54d394ef4865065aef7b5e2baab0f1
Andrei Vasile <andrei.vasile@avira.com>
2020-11-17 20:01:26 +0200

    Full CRUD for customer entity

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -14,11 +14,29 @@ const process = async (req, res, next) => {
   let result = null;
   try {
     switch (method) {
+      case 'customer_list':
+        result = await customerService.findAll();
+        break;
       case 'customer_add':
         result = await customerService.createCustomer(params);
         break;
-    
-      default:
+      case 'customer_find':
+        result = await customerService.findOne(params);
+        break;
+      case 'customer_update':
+        result = await customerService.update(params);
+        break;
+      case 'customer_delete':
+        result = await customerService.destroy(params);
+        break;
+
+      case 'invoice_list':
+        break;
+      case 'invoice_add':
+        break;
+      case 'invoice_update':
+        break;
+      case 'invoice_delete':
         break;
     }
   } catch (err) {
--- src/services/customer.service.js
+++ src/services/customer.service.js
@@ -1,9 +1,36 @@
 const { customerModel } = require('../modules/database');
 
+const findAll = async () => {
+  return customerModel.findAll();
+}
+
+const findOne = async () => {
+  return customerModel.findOne();
+}
+
 const createCustomer = async (customerDto) => {
   return customerModel.create(customerDto);
 };
 
+const update = async (customerDto) => {
+  const {
+    id,
+    ...customerProperties
+  } = customerDto;
+  return customerModel.update(customerProperties, { where: { id } });
+};
+
+const destroy = async (customerDto) => {
+  const {
+    id
+  } = customerDto;
+  return customerModel.destroy({ where: { id } });
+};
+
 module.exports = {
   createCustomer,
+  findAll,
+  update,
+  destroy,
+  findOne,
 }
